bc5a805292ce68c73eb54da830b57b99a7d00f7e
ryota ohashi <ryota.812.skt@gmail.com>
2022-09-01 09:36:53 +0900

    modified

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,50 @@
 import '../scss/style.scss'
+import p5 from "p5";
 
-import Hello from './modules/hello';
+const sketch = (p5) => {
 
-var HOGE = HOGE || {};
+  let imgArray = [];
 
-HOGE = {
-  init: function() {
-    this.setParams();
-    this.bind();
-  },
-  setParams: function() {
 
-  },
-  bind: function() {
-    new Hello();
-  },
+  p5.preload = async() => {
+    await getImage();
+  }
+
+  p5.setup = () => {
+    p5.createCanvas(p5.min(p5.windowHeight - 200, p5.windowHeight - 200), p5.min(p5.windowHeight - 100, p5.windowHeight - 100));
+    console.log(imgArray);
+  };
+
+  p5.draw = () => {
+    p5.background(220);
+  };
+
+  const getImage = async() => {
+    await fetch('https://source.unsplash.com/random')
+      .then((result) => {
+        imgArray.push(result.url);
+      });
+    await fetch('https://source.unsplash.com/random')
+      .then((result) => {
+        imgArray.push(result.url);
+      });
+    await fetch('https://source.unsplash.com/random')
+      .then((result) => {
+        imgArray.push(result.url);
+      });
+    await fetch('https://source.unsplash.com/random')
+      .then((result) => {
+        imgArray.push(result.url);
+      });
+    await fetch('https://source.unsplash.com/random')
+      .then((result) => {
+        imgArray.push(result.url);
+      });
+  }
 };
 
+
 window.addEventListener('DOMContentLoaded', () => {
-  HOGE.init();
+  new p5(sketch);
 });
 
